86aa435de742050ebf260dfc04183cf59c26da73
Stazi-101 <39802426+Stazi-101@users.noreply.github.com>
2019-11-19 15:17:25 +0000

    Update iq.py
    if you dont put this you are cancelled

--- iq.py
+++ iq.py
@@ -1,5 +1,5 @@
 who = input('name pls: ')
-if who == 'harry' or who == 'stazi':
+if who.lower() == 'harry' or who.lower() == 'stazi' or who.lower() == 'anastazie':
     print('15 iq')
 else:
     print('150 iq')
